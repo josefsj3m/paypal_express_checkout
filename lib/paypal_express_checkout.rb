dc13e1a3011c39cc110cff3e570d47cc51a8f393
José Fernando <Jos Fernando josefsmvm@gmail.com>
2017-04-04 14:55:56 -0300

    First commit

---
+++ lib/paypal_express_checkout.rb
@@ -0,0 +1,5 @@
+require "paypal_express_checkout/version"
+
+module PaypalExpressCheckout
+  # Your code goes here...
+end
---
+++ spec/paypal_express_checkout_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe PaypalExpressCheckout do
+  it "has a version number" do
+    expect(PaypalExpressCheckout::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
